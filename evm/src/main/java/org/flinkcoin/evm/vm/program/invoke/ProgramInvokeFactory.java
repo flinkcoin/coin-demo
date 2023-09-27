f61e3e813021619ab56c3472b0f1d802f50910c7
Matej Grasic <matejgrasic@flinkcoin.org>
2023-09-27 10:31:30 +0200

    Adding emv support to branch only

---
+++ evm/src/main/java/org/flinkcoin/evm/vm/program/invoke/ProgramInvokeFactory.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) [2016] [ <ether.camp> ]
+ * This file is part of the ethereumJ library.
+ *
+ * The ethereumJ library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ethereumJ library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ethereumJ library. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.flinkcoin.evm.vm.program.invoke;
+
+import org.flinkcoin.evm.core.Block;
+import org.flinkcoin.evm.core.Repository;
+import org.flinkcoin.evm.core.Transaction;
+import org.flinkcoin.evm.db.BlockStore;
+import org.flinkcoin.evm.vm.DataWord;
+import org.flinkcoin.evm.vm.program.Program;
+
+import java.math.BigInteger;
+
+/**
+ * @author Roman Mandeleil
+ * @since 19.12.2014
+ */
+public interface ProgramInvokeFactory {
+
+    ProgramInvoke createProgramInvoke(Transaction tx, Block block,
+                                      Repository repository, Repository origRepository, BlockStore blockStore);
+
+    ProgramInvoke createProgramInvoke(Program program, DataWord toAddress, DataWord callerAddress,
+                                             DataWord inValue, DataWord inGas,
+                                             BigInteger balanceInt, byte[] dataIn,
+                                             Repository repository, Repository origRepository, BlockStore blockStore,
+                                            boolean staticCall, boolean byTestingSuite);
+
+
+}
